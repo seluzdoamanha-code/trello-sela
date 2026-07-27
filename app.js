173af07abf93dabc129b82586604d422087eb1ce
seluzdoamanha-code <seluzdoamanha@gmail.com>
2026-07-26 23:40:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // IMPORTANTE: Preencha com as suas credenciais do Supabase
-const SUPABASE_URL = 'https://aymdooyafimliiggxeqs.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://aymdooyafimliiggxeqs.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5bWRvb3lhZmltbGlpZ2d4ZXFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMDUxNDksImV4cCI6MjEwMDY4MTE0OX0.-NBhiyGDlrWq4QKNLx9Ll5GlIk0mV_rBWnr0vdbUCOU';
 
 let supabase;
